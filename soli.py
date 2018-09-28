f88f3d0e31783675e5a6f9baf9ebe08c450cbd58
miguel-amezcua <43039566+miguel-amezcua@users.noreply.github.com>
2018-09-28 12:24:43 -0500

    Update soli.py
    Corrección de problemas con conectores de base de datos y comunicación de los menús y funciones. 
    -se dejo de utilizar mysql y se inicio el uso de sqlite3 a petición de uno de los miembros del equipo. 
    -Se corrigieron problemas de reconocimiento de variables, en este código son mas sencillas y fáciles de seguir
    -Errores en el menú, interfaz para uso de usuario corregidos

--- soli.py
+++ soli.py
@@ -1,146 +1,157 @@
-import mysql.connector
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Creamos diccionario
-dato = {
-    'user' : 'root',
-    'password' : ' ',
-    'database' : 'tutorial ',
-    'host' : '127.0.0.1'
-    }
-conexion = mysql.connector.connect(** dato)
-conexion.close()
-
-cursor = conexion.cursor () #creamos el cursor
+import sqlite3
+import os
+import sys
+import time
 
 def Menu():
-    import os,sys
-    print ("Menu provicional de acciones. ")
-    print("")
-    print("1.- agregar ")
-    print("2.- modificar")
-    print("3.- eliminar ")
-    print("4.- mostrar")
-    print("5.- ver variables ")
-    print("6.- salir ")
-    print("")
-    try:
-        OP=int(input("intruduce una opcion valida:"))
-    except:
+     print ("Menu provicional de acciones. ")
+     print("")
+     print("1.- agregar ")
+     print("2.- modificar")
+     print("3.- eliminar ")
+     print("4.- mostrar")
+     print("5.- salir ")
+     print("")
+     try:
+         op=int(input("intruduce una opcion valida:"))
+     except:
          print("No es una opcion valida")
          print("")
          Menu()
 
-    os.system('cls')
-    if op==1:
-        agregar()
-    elif op==2:
-        modificar()
-    elif op==3:
-        eliminar()
-    elif op==4:
-        mostrar()
-    elif op==5:
-        salir()
-    else:
-        print("ingrese un numero del menu")
-        Menu()
+     os.system('cls')
+     if op==1:
+         Agregar()
+     elif op==2:
+         Modificar()
+     elif op==3:
+         Eliminar()
+     elif op==4:
+         Mostrar()
+     elif op==5:
+         Salir()
+     else:
+         print("ingrese un numero del menu")
+         Menu()
 
-def agregar():
-    import os.sys.mysql
-    con = mysql.connector('')
-    print("Agregar")
-    print("")
-
-    _date=input("inrese un valor")
-    _time=input("ingrese un valor ")
-    watts_now=input("ingrese un valor ")
-    day_kmh=input("ingrese un valor ")
-    total_kmh=input("ingrese un valor ")
-    os.system('cls')
-    cursor=com.cursor()
-    con.close()
-    Menu()
-
-def salir():
-    import os.system
-    print("has elegido salir ")
-
-def ver():
-    import os,sys,mysql
-    con=mysql.connector('lecturas_')
-    cursor=con.cursor()
-    cursor.execute("select * from lecturas_")
-    print("esta en opcion ver ")
+def Mostrar():
+    conn = sqlite3.connect(r"C:\Users\practicas3.jalisco\Desktop\db2\mynewdb.db")
+    cursor = conn.cursor()
+    print ("esto indica que nos conectamos""\n") 
+    a=cursor.execute("select id_lecturas, __date, _time, watts_now, day_kmh, total_kmh FROM lecturas__")
     print(" ")
-    print("id Cod      \t  Nombre        \t Lectura")
-    print("=================================================")
+    print("\tid Cod \t  fecha  \t Hora    watts \t lect/dia  total_kmh ")
+    print("\t==============================================================")
     for lecturas_ in cursor:
-        lectura_='\t'+str(lecturas_[0])+'\t'+str(lecturas_[1])+'\t'+str(lecturas_[2])+'\t'+str(lecturas_[3])+'\t'+str(lecturas_[4])+'\t'+str(lecturas_[5])
-        print(str(lecturas_))
-    con.close
-    print(" ")
+        #print("esto indica que a mediados estoy bien ")
+        lecturas_='\t'+str(lecturas_[0])+'\t'+str(lecturas_[1])+'\t'+str(lecturas_[2])+'\t'+str(lecturas_[3])+'   \t '+str(lecturas_[4])+'\t\t'+str(lecturas_[5])
+        print (str(lecturas_))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print ("\n")
+   
+    time.sleep(7)
+    os.system('clear')
     Menu()
-              
-def eliminar():
-    import ows,sys,mysql
-    con=mysql.connector('lecturas_')
-    cursor=con.cursor()
-    cursor.execute("select * from lecturas_")
+
+def Agregar():
+    conn = sqlite3.connect(r"C:\Users\practicas3.jalisco\Desktop\db2\mynewdb.db")
+    cursor = conn.cursor()
+    print("Opcion Agregar")
+    print("\n")
+    __date=input("Ingrese un valor para fecha: ")
+    _time=input("Ingrese un valor para hora: ")
+    watts_now=int(input("Ingrese un valor: "))
+    day_kmh=int(input("Ingrese un valor consumo: "))
+    total_kmh=int(input("Ingrese un valor total medido: "))
+    sentencia = "insert into lecturas__(__date, _time, watts_now, day_kmh, total_kmh)values(?,?,?,?,?)"
+    cursor.execute(sentencia, [__date, _time, watts_now, day_kmh, total_kmh])
+    print ("Datos guardados correctamente")
+
+    os.system('cls')
+    conn.commit()
+    cursor.close()
+    conn.close()
+    
+    print ("\n")
+    time.sleep(3)
+    os.system('clear')
+    Menu()
+    
+def Eliminar():
+    conn = sqlite3.connect(r"C:\Users\practicas3.jalisco\Desktop\db2\mynewdb.db")
+    cursor = conn.cursor()
+    cursor.execute("select * from lecturas__")
     print("Esta en opcion Eliminar ")
     print(" ")
-    print("id Cod      \t  Nombre        \t Lectura")
-    print("=================================================")
+    print("\tid Cod \t  fecha  \t Hora    watts \t lect/dia  total_kmh ")
+    print("\t==============================================================")
     for lecturas_ in cursor:
-        lecturas_.append(lecturas_)
-    lecturas_='\t'+str(lecturas_[0])+'\t'+str(lecturas_[1])+'\t'+str(lecturas_[2])+'\t'+str(lecturas_[3])+'\t'+str(lecturas_[4])+'\t'+str(lecturas_[5])
-    print(str(lecturas_))
+        #print("esto indica que a mediados estoy bien ")
+        lecturas_='\t'+str(lecturas_[0])+'\t'+str(lecturas_[1])+'\t'+str(lecturas_[2])+'\t'+str(lecturas_[3])+'   \t '+str(lecturas_[4])+'\t\t'+str(lecturas_[5])
+        print (str(lecturas_))
     print("")
-    print("decea eliminar alguna entrada:")
+    print("Decea eliminar alguna entrada:")
     cod=input("digita el codigo de la entrada que decea eliminar:")
-    sql="delete from lecturas_ where cod ="+cod
-    cursor.execute(mysql)
-    con.commit()
-    con.close()
-    os.system('cls')
-    print("el producto fue eliminado")
-    print(" ")
+    sentencia="delete from lecturas__ where id_lecturas = ?; "
+    cursor.execute(sentencia, [cod])
+    print ("la entrafa fue eliminada ")
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print ("\n")
+    time.sleep(2)
+    os.system('clear')
     Menu()
 
 def Modificar():
-    import ows,sys,mysql
-    con=mysql.connector('lecturas_')
-    cursor=con.cursor()
-    cursor.execute("select * from lecturas_")
+    info=[]
+    conn = sqlite3.connect(r"C:\Users\practicas3.jalisco\Desktop\db2\mynewdb.db")
+    cursor = conn.cursor()
+    cursor.execute("select * from lecturas__")
     print("Esta en opcion Eliminar ")
     print(" ")
-    print("id Cod      \t  Nombre        \t Lectura")
-    print("=================================================")
-    for lecturas_ in cursor:
-        lecturas_.append(lecturas_)
-    lecturas_='\t'+str(lecturas_[0])+'\t'+str(lecturas_[1])+'\t'+str(lecturas_[2])+'\t'+str(lecturas_[3])+'\t'+str(lecturas_[4])+'\t'+str(lecturas_[5])
-    print(str(lecturas_))
+    print("\tid Cod \t  fecha  \t Hora    watts \t lect/dia  total_kmh ")
+    print("\t==============================================================")
+    for lecturas__ in cursor:
+        info.append(lecturas__)
+        #print("esto indica que a mediados estoy bien ")
+        lecturas__='\t'+str(lecturas__[0])+'\t'+str(lecturas__[1])+'\t'+str(lecturas__[2])+'\t'+str(lecturas__[3])+'   \t '+str(lecturas__[4])+'\t\t'+str(lecturas__[5])
+        print (str(lecturas__))
     print("")
     print("Decea Modificar algun valor:")
-    con=input("digita el codigo de la entrada que decea Modificar:")
-    for productos in lecturas_
-	if int (productos[0])== int (cod):
-		_date=productos[1]
-		_time=productos[2]
-		watts_now=productos[3]
-		day_kmh=productos[4]
-		total_kmh=productos[5]
-		encontrado=true
-		break
-	_date=input("ingrese el nuevo valor:"+str(date)+":")
-	-time=input("ingrese un nuevo valor"+str(time)+":")
-	watts_now=input("ingrese un nuevo valor"+str(watts_now)+":")	
-	day_kmh =input("ingrese un nuevo valor"+str(day_kmh)+":")
-	total_kmh =input("ingrese un nuevo valor"+str(total_kmh":")
-	mysql="update lecturas_ set _date='"+_date+"',_time='"+_time+"'watts_now='"+watts_now+"'day_kmh'"+day_kmh+"'total_kmh'"+total_kmh+"' where cod 
-	cursor.execute(mysql)
-	con.commit()
-	con.clese()
-	os.system("cls")
-	print("el producto a sido modificado !!!")
-	print (" ")
-	Menu()
+    cod=input("digita el codigo de la entrada que decea Modificar:")
+    for lecturas__ in info:
+        if int(lecturas__[0])== int (cod):
+                __date=lecturas__[1]
+                _time=lecturas__[2]
+                watts_now=lecturas__[3]
+                day_kmh=lecturas__[4]
+                total_kmh=lecturas__[5]
+                encontrado=True
+        break
+    __date=input("ingrese el nuevo valor:")
+    _time=input("ingrese un nuevo valor:")
+    watts_now=input("ingrese un nuevo valor:")
+    day_kmh =input("ingrese un nuevo valor:")
+    total_kmh =input("ingrese un nuevo valor:")
+    sentencia="update lecturas__ set __date='"+__date+"',_time='"+_time+"',watts_now='"+watts_now+"',day_kmh='"+day_kmh+"',total_kmh='"+total_kmh+"' where id_lecturas =?"
+    cursor.execute(sentencia, [cod])
+    print ("el producto fue modificado")
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print ("\n")
+    time.sleep(2)
+    os.system('clear')
+    Menu()
+
+def Salir():
+    print("has elegido salir ") 
+     
+Menu()
+
